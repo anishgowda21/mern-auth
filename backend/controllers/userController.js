e474dcf27e57a90ec4dc805683bb2aa375322433
Anish Gowda <anishgowda43@gmail.com>
2024-07-24 19:47:58 +0530

    added all routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,11 +4,43 @@ import asyncHandler from 'express-async-handler';
 //route POST /api/users/auth
 //@access public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(401);
-    throw new Error("something went wrong");
+    res.status(200).json({ message: "Auth User" });
+});
+
+
+//@desc Register new user
+//route POST /api/users
+//@access public
+const registerUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Register User" });
+});
+
+//@desc Logout User
+//route POST /api/users/logout
+//@access public
+const logoutUser = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Logout User" });
+});
+
+
+//@desc Get user profile
+//route GET /api/users/profile
+//@access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Auth User" });
+});
+
+//@desc Update user profile
+//route PUT /api/users/profile
+//@access Privaye
+const updateUserProfile = asyncHandler(async (req, res) => {
     res.status(200).json({ message: "Auth User" });
 });
 
 export {
-    authUser
+    authUser,
+    registerUser,
+    logoutUser,
+    getUserProfile,
+    updateUserProfile
 }
