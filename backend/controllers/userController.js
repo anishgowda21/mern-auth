5e4160d7eb09929e69336ac68aeb1f0c36a47bfe
Anish Gowda <anishgowda43@gmail.com>
2024-07-24 19:39:46 +0530

    added routes and errorhandler

---
+++ backend/controllers/userController.js
@@ -0,0 +1,14 @@
+import asyncHandler from 'express-async-handler';
+
+//@desc Auth user/set token
+//route POST /api/users/auth
+//@access public
+const authUser = asyncHandler(async (req, res) => {
+    res.status(401);
+    throw new Error("something went wrong");
+    res.status(200).json({ message: "Auth User" });
+});
+
+export {
+    authUser
+}
